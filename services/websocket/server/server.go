967e8c75b9c5d548bace4a3981fb76e768a4ce2c
SaltIncluded <43348526+casperfj@users.noreply.github.com>
2025-06-27 21:34:48 +0200

    added mutex to maps and heartbeat

--- services/websocket/server/server.go
+++ services/websocket/server/server.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
@@ -16,17 +18,99 @@ type session struct {
 	Viewers []uuid.UUID
 }
 
-type actor struct {
+func (s session) Close() {
+	sessionLock.Lock()
+	delete(sessions, s.ID)
+	sessionLock.Unlock()
+}
+
+type client struct {
+	ID        uuid.UUID
 	SessionID uuid.UUID
 	Conn      *websocket.Conn
 }
 
-var sessions = make(map[uuid.UUID]session)
-var actors = make(map[uuid.UUID]actor)
+func (c client) Disconnect() {
+	if c.Conn != nil {
+		c.Conn.Close()
+	}
+	if c.SessionID != uuid.Nil {
+		sessionLock.RLock()
+		session, exists := sessions[c.SessionID]
+		sessionLock.RUnlock()
+		if exists && session.OwnerID == c.ID {
+			session.Close()
+		}
+	}
+	clientLock.Lock()
+	delete(clients, c.ID)
+	clientLock.Unlock()
+}
+
+func (c *client) SetSessionID(sessionID uuid.UUID) {
+	c.SessionID = sessionID
+	clientLock.Lock()
+	clients[c.ID] = *c
+	clientLock.Unlock()
+}
+
+var (
+	pingPeriodDuration = time.Second * time.Duration(10)
+	pongWaitDuration   = pingPeriodDuration + time.Second*time.Duration(10)
+)
+
+func (c *client) StartHeartbeat(conn *websocket.Conn) chan struct{} {
+	_ = conn.SetReadDeadline(time.Now().Add(pongWaitDuration))
+	conn.SetPongHandler(func(string) error {
+		_ = conn.SetReadDeadline(time.Now().Add(pongWaitDuration))
+		return nil
+	})
+
+	heartbeat := make(chan struct{})
+	go func() {
+		ticker := time.NewTicker(pingPeriodDuration)
+		defer ticker.Stop()
+		for {
+			select {
+			case <-ticker.C:
+				conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+				if err := conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+					log.Printf("Ping failed for %s: %v", c.ID, err)
+					c.Disconnect()
+					close(heartbeat)
+					return
+				}
+			case <-heartbeat:
+				return
+			}
+		}
+	}()
+
+	return heartbeat
+}
+
+var (
+	sessions    = make(map[uuid.UUID]session)
+	sessionLock sync.RWMutex
+	clients     = make(map[uuid.UUID]client)
+	clientLock  sync.RWMutex
+)
 
 var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool { // TODO: remove this in prod
-		return true
+	CheckOrigin: func(r *http.Request) bool {
+		log.Printf("Origin: %s", r.Header.Get("Origin"))
+		return true // TODO: remove this in prod
+		/*origin := r.Header.Get("Origin")
+		trustedOrigins := []string{
+			"https://example.com",
+			"https://another-trusted-origin.com",
+		}
+		for _, trustedOrigin := range trustedOrigins {
+			if origin == trustedOrigin {
+				return true
+			}
+		}
+		return false*/
 	},
 }
 
@@ -39,12 +123,21 @@ func handleWebsocket(w http.ResponseWriter, req *http.Request) {
 	defer conn.Close()
 
 	var actorID uuid.UUID
+	var heartbeat chan struct{}
 	for { // Reroll until unique UUID
 		actorID = uuid.New()
-		if _, exists := actors[actorID]; !exists {
-			actors[actorID] = actor{
+		clientLock.RLock()
+		_, exists := clients[actorID]
+		clientLock.RUnlock()
+		if !exists {
+			clientLock.Lock()
+			var newClient client = client{
+				ID:   actorID,
 				Conn: conn,
 			}
+			heartbeat = newClient.StartHeartbeat(conn)
+			clients[actorID] = newClient
+			clientLock.Unlock()
 			break
 		}
 	}
@@ -54,6 +147,8 @@ func handleWebsocket(w http.ResponseWriter, req *http.Request) {
 	for {
 		messageType, msg, err := conn.ReadMessage()
 		if err != nil {
+			clients[actorID].Disconnect()
+			close(heartbeat)
 			log.Println("Error reading message:", err)
 			break
 		}
@@ -85,25 +180,30 @@ func handleMessage(authorID uuid.UUID, messageType int, msg []byte) {
 	switch msgData.Event {
 	case "create_session":
 		var sessionID uuid.UUID
-		if actors[authorID].SessionID != uuid.Nil { // Already has a session
-			sessionID = actors[authorID].SessionID
+		clientLock.RLock()
+		var client client = clients[authorID]
+		if client.SessionID != uuid.Nil { // Already has a session
+			sessionID = client.SessionID
+			clientLock.RUnlock()
 		} else {
+			clientLock.RUnlock()
+			sessionLock.RLock()
 			for { // Reroll until unique UUID
 				sessionID = uuid.New()
 				if _, exists := sessions[sessionID]; !exists {
 					break
 				}
 			}
+			sessionLock.RUnlock()
 			newSession := session{
 				ID:      sessionID,
 				OwnerID: authorID,
 				Viewers: []uuid.UUID{},
 			}
+			sessionLock.Lock()
 			sessions[sessionID] = newSession
-			actors[authorID] = actor{
-				SessionID: sessionID,
-				Conn:      actors[authorID].Conn,
-			}
+			sessionLock.Unlock()
+			client.SetSessionID(sessionID)
 		}
 
 		resMsgData := message{
@@ -115,22 +215,22 @@ func handleMessage(authorID uuid.UUID, messageType int, msg []byte) {
 			log.Println("Error marshalling response message:", err)
 			return
 		}
-		if err := actors[authorID].Conn.WriteMessage(messageType, resMsg); err != nil {
+		if err := client.Conn.WriteMessage(messageType, resMsg); err != nil {
 			log.Println("Error writing message:", err)
-			delete(actors, authorID)
+			client.Disconnect()
 			return
 		}
 	default:
 		log.Printf("Unhandled event: %s", msgData.Event)
 	}
 
-	debugState()
+	debugState() // For debugging purposes, remove in production
 }
 
 func debugState() {
-	log.Println("Current actors:")
-	for id, actor := range actors {
-		log.Printf("Actor ID: %s, Session ID: %s", id, actor.SessionID)
+	log.Println("Current clients:")
+	for id, client := range clients {
+		log.Printf("Client ID: %s, Session ID: %s", id, client.SessionID)
 	}
 	log.Println("\nCurrent sessions:")
 	for id, session := range sessions {
